2d7b5dc9a9a0121642ef490cecf6c90eb6ce7093
felipepar <felipe.parreira@atlasgr.com.br>
2020-08-20 21:32:10 -0300

    configured settings for deployment

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = 'u$s%3ypg#$7m(lh0#1z59hst5)335l@+5dj%)(o+&jkvg&g_8t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = booll(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+STATIC_ROOT = 'static/'
